e75e0d24f48a2f2686410aea51337d02351ab791
Tajeshwar Khara <tkhara@gmail.com>
2019-12-14 17:57:20 +0530

    Stub 3

---
+++ src/main/java/ImageHoster/model/User.java
@@ -0,0 +1,13 @@
+package ImageHoster.model;
+
+
+public class User {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private UserProfile profile;
+
+   //Genarate getters and setters for all the attributes
+}
+
---
+++ src/main/java/ImageHoster/model/UserProfile.java
@@ -0,0 +1,13 @@
+package ImageHoster.model;
+
+
+public class UserProfile {
+
+    private Integer id;
+    private String fullName;
+    private String emailAddress;
+    private String mobileNumber;
+
+
+    //Generate getters and setters for all the attributes
+}
